78df2e90442e0741e2c4589b35f97bd0cd201577
Rifars <ramlifr12@gmail.com>
2020-12-24 17:55:54 +0700

    resolve code

--- assets/script/main.js
+++ assets/script/main.js
@@ -11,20 +11,20 @@ function dataCard(dCard){
     let data = `<div class="row row${dCard}">
     <!-- row header -->
     <div class="row-header" >
-        <img src="assets/images/card/Burger${dCard}.png" alt="burger${dCard}" class="Burger${dCard}" data-aos="fade-left">
+        <img src="assets/images/card/Burger${dCard}.png" alt="burger${dCard}" class="Burger${dCard}">
     </div>
     <!-- end row header -->
 
     <!-- row description -->
     <div class="row-description">
-        <h1 data-aos="fade-left" data-aos-delay="200">lorem ipsum dolor</h1>
-        <p data-aos="fade-left" data-aos-delay="200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, quod expedita.</p>
+        <h1>lorem ipsum dolor</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, quod expedita.</p>
     </div>
     <!-- end row description -->
 
     <!-- row button -->
     <div class="row-button">
-        <button type="button" data-aos="fade-left" data-aos-delay="300">order now</button>
+        <button type="button" >order now</button>
     </div>
     <!-- end row button -->
 </div>`
